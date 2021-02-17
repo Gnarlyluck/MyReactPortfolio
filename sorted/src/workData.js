14d54ed55eb459593ae16d7ae46efa942f58162d
Gnarlyluck <schuylerluckey@gmail.com>
2021-02-16 20:11:44 -0800

    added work data to be added to as more projects are ready for presentation

---
+++ sorted/src/workData.js
@@ -0,0 +1,31 @@
+export default {
+    projects: [
+        {
+          id: 1, //DO NOT CHANGE THIS 
+          title: "Cocktail Social",
+          para:
+            "This app is dedicated to sharing your favorite cocktails or sharing your new experiments and getting feedback. Here you can find out what your friends and peers are mixing up behind the bar.", // Add Your Service Type Here
+          imageSrc:
+            "https://www.wsbtv.com/resizer/44JlN-VtBRqH1jP4myKUaY-RzXk=/1200x675/arc-anglerfish-arc2-prod-cmg.s3.amazonaws.com/public/PV3SJNEXFOPT7AMSJGEV5MCSKI.jpg",
+          url: "https://cocktailsocial.herokuapp.com/",
+        },
+        {
+          id: 2, //DO NOT CHANGE THIS 
+          title: "Pluck Pluck Choose", 
+          para:
+            "Pluck Pluck Choose is a Tinder clone of sorts where you either pluck an animal pic or chuck a animal pic, check out the compare option for some good laughs.", // Add Your Service Type Here
+          imageSrc:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAAQb6cD9s9TTk6ZxvohxA3byGJQToHPv3tg&usqp=CAU",
+          url: "https://pluck-pluck-choose.herokuapp.com/",
+        },
+        {
+          id: 3, //DO NOT CHANGE THIS 
+          title: "Luckey's Cocktail Finder", 
+          para:
+            "This website utilizes the thecocktaildb API to allow users to search for the ingredient list and build process for almost any cocktail you can think of.", // Add Your Service Type Here
+          imageSrc:
+            "https://i2.wp.com/abarabove.com/wp-content/uploads/2018/12/P1-Vegan-Unfriendly-Ingredients.jpg?w=1080&ssl=1",
+          url: "http://quixotic-name.surge.sh/index.html",
+        },
+      ],
+}

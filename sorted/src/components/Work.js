439f2d5879f65bd461aba828c6d9bfbbcdfe7bc3
Gnarlyluck <schuylerluckey@gmail.com>
2021-02-16 20:08:13 -0800

    added work component to app.js

---
+++ sorted/src/components/Work.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Card from "./Card"
+import Fade from "react-reveal/Fade"
+import data from '../workData'
+// import styles from '../scss/work.scss'
+const Work = () => {
+  return (
+    <div className="section" id="work">
+      <div className="container">
+        <div className="work-wrapper">
+          <Fade bottom>
+            <h1>Work</h1>
+          </Fade>
+
+          <div className="grid">
+            <Fade bottom cascade>
+              {data.projects.map(project => (
+                <Card
+                  key={project.id}
+                  heading={project.title}
+                  paragraph={project.para}
+                  imgUrl={project.imageSrc}
+                  projectLink={project.url}
+                ></Card>
+              ))}
+            </Fade>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Work
--- sorted/src/pages/App.js
+++ sorted/src/pages/App.js
@@ -2,11 +2,13 @@ import '../styles/App.css';
 import Home from './Home'
 import { withRouter } from 'react-router-dom'
 import Layout from '../pages/Layout'
+import Work from '../components/Work' 
 
 function App() {
   return (
   <Layout>
     <Home></Home>
+    <Work></Work>
   </Layout>
   );
 }

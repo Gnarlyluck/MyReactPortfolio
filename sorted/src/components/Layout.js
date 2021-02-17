706da975c73968f3eda33c67c5a7325a244a6e60
Gnarlyluck <schuylerluckey@gmail.com>
2021-02-17 12:31:34 -0800

    added a layout component

---
+++ sorted/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Nav from "../components/Nav"
+import styles from '../scss/mains.scss'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>{children}</main>
+    </div>
+  )
+}
+export default Layout
